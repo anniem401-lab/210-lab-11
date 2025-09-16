3a8a1db49e8f04e6c9c2ad6cb891c79b16c8b67e
Annie <annmora21@gmail.com>
2025-09-16 15:21:59 -0700

    v-0.1

---
+++ l-11.cpp
@@ -0,0 +1,22 @@
+// COMSC_210 | Lab 11 | Annie Morales
+// IDE used: Visual Studio Code
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Holds order information
+struct Orders
+{
+    int orderNumber;
+    int itemsOrdered;
+    string customerName;
+    double totalAmount;
+};
+
+
+int main() 
+{
+
+    return 0;
+}
