f51ddc014be5192a7dbc2b77720e31134cf96a6a
Annie <annmora21@gmail.com>
2025-09-16 15:37:55 -0700

    v-0.2

--- l-11.cpp
+++ l-11.cpp
@@ -5,18 +5,42 @@
 #include <string>
 using namespace std;
 
+const int OrderSize = 3; 
+
 // Holds order information
-struct Orders
+struct Customer
 {
+    int * orders;
     int orderNumber;
     int itemsOrdered;
     string customerName;
     double totalAmount;
+
+    ~Customer() // Destructor 
+    {
+        // Placeholder for code.
+    }
 };
 
+void inputOrders(Customer *orders) 
+{
+    for (int i = 0; i < OrderSize; i++) 
+    {
+        cout << "Enter order number: ";
+        cin >> orders[i].orderNumber;
+        cout << "Enter items ordered: ";
+        cin >> orders[i].itemsOrdered;
+        cout << "Enter customer name: ";
+        cin.ignore(); // To ignore the newline character left in the buffer
+        getline(cin, orders[i].customerName);
+        cout << "Enter total amount: ";
+        cin >> orders[i].totalAmount;
+    }
+}
 
 int main() 
 {
+
 
     return 0;
 }
